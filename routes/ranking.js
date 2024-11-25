76ac9e899b03a1a59c607737d00ceb598e9644be
Glenda Alves <107262765+glendalvesx@users.noreply.github.com>
2024-11-24 22:47:29 -0300

    Update ranking.js

--- routes/ranking.js
+++ routes/ranking.js
@@ -17,15 +17,30 @@ router.post('/', async (req, res) => {
                 pontuacao,
                 tempo,
                 posicao,
-            },
-        });
+            }
+        })
+
+        const rankings = await prisma.ranking.findMany({
+            orderBy: [
+                { pontuacao: 'desc'}, //maior pontuação primeiro
+                { tempo: 'asc'}, // menor tempo primeiro
+            ]
+        })
+
+        // atualiza a posição de cada jogador
+        for (let i = 0; i < rankings.length; i++){
+            await prisma.ranking.update({
+                where: { id: rankings[i].id},
+                data: { posicao: i + 1}
+            })
+        }
 
         res.status(200).json(novoRanking);
     } catch (err) {
         console.error('Erro no servidor:', err);
         res.status(500).json({ message: 'Erro no servidor, tente novamente.' });
     }
-});
+})
 
 // Rota GET para buscar todos os rankings
 router.get('/', async (req, res) => {
